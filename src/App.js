a3420480280e9d5a257a0108e127995f6ba60cc1
EC2 Default User <ec2-user@ip-172-31-54-239.ec2.internal>
2021-04-11 22:26:02 +0000

    done link

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './styles/main.css';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 /* コンポーネント */
 import Student from './components/Student';
-
+import Edit from './components/Edit';
 function App() {
   return (
-    <div className="container is-fluid">
-      <Student />
-    </div>
+    <Router>
+      <div className="container is-fluid">
+        <Switch>
+          <Route exact path="/">
+            <Student />
+          </Route>
+          <Route path="/edit">
+            <Edit />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
---
+++ src/components/Edit.js
@@ -0,0 +1,16 @@
+const Edit = () => {
+    return (
+     <article className="panel is-danger">
+          <div className="panel-heading">
+            <span class="icon-text">
+              <span class="icon">
+                <i class="fas fa-calendar-check"></i>
+              </span>
+              <span>EDIT A STUDENT </span>
+            </span>
+          </div>   
+    </article>
+    );
+}
+
+export default Edit;
